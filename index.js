b6a33931923107169f2a22f8bf842295a0bc105f
Aaron Donelson <aaron.donelson23@gmail.com>
2021-10-04 09:43:41 -0400

    all functions are looped in with each other.

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const Employee = require('../lib/Employee');
 const Intern = require("../lib/Intern");
 const Manager = require("../lib/Manager");
 
-
 //Employee must have Name , ID, email, getName(), getID, getEmail(), getRole(returns employee)
 
 //Manager, inlcuded employee + office number override getRole(show manager)
@@ -17,103 +16,180 @@ const Manager = require("../lib/Manager");
 
 //add validation to each prompt to ensure proper user input
 
-//each Employee created will become a card. 
-function createManager(){
-    inquier.prompt([
-        {   
+//each Employee created will become a card.
+createManager();
+
+function createManager() {
+    inquirer.prompt([{
             type: 'input',
             message: "What is your name?",
             name: 'Name',
-            validate: (value) =>{ if(value){return true} else {return 'I need you to enter your name'}}           
+            validate: (value) => {
+                if (value) {
+                    return true
+                } else {
+                    return 'I need you to enter your name'
+                }
+            }
         },
-        {   
+        {
             type: 'input',
             message: "What is your id number",
             name: 'id',
-            validate: (value) =>{ if(value){return true} else {return 'I need you to enter your ID number'}}           
+            validate: (value) => {
+                if (value) {
+                    return true
+                } else {
+                    return 'I need you to enter your ID number'
+                }
+            }
         },
-        {   
+        {
             type: 'input',
             message: "What is your Email address?",
             name: 'Email',
-            validate: (value) =>{ if(value){return true} else {return 'I need you to enter your Email'}}           
+            validate: (value) => {
+                if (value) {
+                    return true
+                } else {
+                    return 'I need you to enter your Email'
+                }
+            }
         },
-        {   
+        {
             type: 'input',
             message: "what is your office number?",
-            name: 'role',
-            validate: (value) =>{ if(value){return true} else {return 'I need you to enter an office number'}}           
+            name: 'office',
+            validate: (value) => {
+                if (value) {
+                    return true
+                } else {
+                    return 'I need you to enter an office number'
+                }
+            }
         },
-    ]);   
+    ]);
+    addNewEmployee();
 }
 
-function addNewEmployee(){
-    inquier.prompt([
-        {   
+
+function addNewEmployee() {
+    inquirer.prompt([{
             type: 'list',
             message: "Would you like to add another employee?",
             name: 'new',
-            choices:["Yes", "No"],
-            validate: (value) =>{ if(value){return true} else {return 'I need you to choose yes or no'}}           
-        },
-    ])
+            choices: ["Yes", "No"],
+            validate: (value) => {
+                if (value) {
+                    return true
+                } else {
+                    return 'I need you to choose yes or no'
+                }
+            }
+        }, ])
+        .then(answers => {
+            if (answers.new === "Yes") {
+                createEmployee();
+            } else {
+                createIndex();
+            }
+        })
 }
 
-function createEmployee(){
-inquier.prompt([
-    {   
-        type: 'input',
-        message: "What is your name?",
-        name: 'Name',
-        validate: (value) =>{ if(value){return true} else {return 'I need you to enter your name'}}           
-    },
-    {   
-        type: 'input',
-        message: "What is your id number",
-        name: 'id',
-        validate: (value) =>{ if(value){return true} else {return 'I need you to enter your ID number'}}           
-    },
-    {   
-        type: 'input',
-        message: "What is your Email address?",
-        name: 'Email',
-        validate: (value) =>{ if(value){return true} else {return 'I need you to enter your Email'}}           
-    },
-    {   
-        type: 'list',
-        message: "What is your role?",
-        name: 'role',
-        choices: ['Manager', "Engineer", "Intern"],
-        validate: (value) =>{ if(value){return true} else {return 'I need you to choose a role'}}           
-    },
-])
-.then(answer => { 
-    const role = answer.role;
-    
-})
+function createEmployee() {
+    inquirer.prompt([{
+                type: 'input',
+                message: "What is your name?",
+                name: 'Name',
+                validate: (value) => {
+                    if (value) {
+                        return true
+                    } else {
+                        return 'I need you to enter your name'
+                    }
+                }
+            },
+            {
+                type: 'input',
+                message: "What is your id number",
+                name: 'id',
+                validate: (value) => {
+                    if (value) {
+                        return true
+                    } else {
+                        return 'I need you to enter your ID number'
+                    }
+                }
+            },
+            {
+                type: 'input',
+                message: "What is your Email address?",
+                name: 'Email',
+                validate: (value) => {
+                    if (value) {
+                        return true
+                    } else {
+                        return 'I need you to enter your Email'
+                    }
+                }
+            },
+            {
+                type: 'list',
+                message: "What is your role?",
+                name: 'role',
+                choices: ["Engineer", "Intern"],
+                validate: (value) => {
+                    if (value) {
+                        return true
+                    } else {
+                        return 'I need you to choose a role'
+                    }
+                }
+            },
+        ])
+        .then(answers => {
+            if (answers.role == 'Engineer') {
+                createEngineer();
+            } else {
+                createIntern();
+            }
+
+        })
 
 }
 
-function createEngineer(){
-    inquier.prompt([
-    {   
+function createEngineer() {
+    inquirer.prompt([{
         type: 'input',
         message: "What is your GitHub username?",
         name: 'github',
-        validate: (value) =>{ if(value){return true} else {return 'I need you to enter your github username'}}           
-    }, 
-])
+        validate: (value) => {
+            if (value) {
+                return true
+            } else {
+                return 'I need you to enter your github username'
+            }
+        }
+    }, ])
+    addNewEmployee();
 }
 
-function createIntern(){
-    inquier.prompt([
-    {   
+function createIntern() {
+    inquirer.prompt([{
         type: 'input',
         message: "What school do you go to?",
         name: 'school',
-        validate: (value) =>{ if(value){return true} else {return 'I need you to enter your school'}}           
-    }, 
-])
+        validate: (value) => {
+            if (value) {
+                return true
+            } else {
+                return 'I need you to enter your school'
+            }
+        }
+    }, ])
+    addNewEmployee();
 }
 
-fs.writeFile
+function createIndex() {
+    fs.writeFile
+}
